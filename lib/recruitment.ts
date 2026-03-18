17b67846f688861ce9df0a78d235068b4d813aba
Fu Hoang <fuhoang84@googlemail.com>
2026-03-18 18:59:07 +0000

    Refactor recruitment components

---
+++ lib/recruitment.ts
@@ -0,0 +1,27 @@
+export const RECRUITMENT_EMAIL_FALLBACK = "fuhoang84@googlemail.com";
+
+export const cleaningExperienceOptions = [
+  "Private homes",
+  "Agency cleaning",
+  "Office cleaning",
+  "End of tenancy",
+  "Deep cleaning",
+  "Hospitality cleaning",
+];
+
+export const workingDayOptions = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
+export const eligibilityOptions = [
+  "I am eligible to work in the UK.",
+  "I can travel to local cleaning jobs.",
+  "I am comfortable working in customers' homes.",
+  "I am happy to complete checks and onboarding.",
+];
